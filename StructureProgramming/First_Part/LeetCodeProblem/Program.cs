7d6da5e8124c5608465a7316ecd2287d87aa0745
KHALID SIFULLAH <khalidsifullah97@gmail.com>
2021-04-07 23:33:46 +0600

    again

---
+++ StructureProgramming/First_Part/LeetCodeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCodeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] num = new int[5];
+            for (i = 0; i < num.Length; i++)
+            {
+                Convert.ToInt32(Console.ReadLine(num[i]));
+            }
+        }
+    }
+}
